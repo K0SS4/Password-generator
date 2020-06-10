9002d54c17597a75ecfca55529e3baa65bd9ad8f
K0SS4 <kontakt.kkossakowski@gmail.com>
2020-06-10 18:41:14 +0200

    Generating password only for all checkboxes checked, length working

--- PasswordGenerator/MainWindow.xaml.cs
+++ PasswordGenerator/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
+using System.Windows.Media.TextFormatting;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace PasswordGenerator
 {
@@ -20,9 +10,95 @@ namespace PasswordGenerator
     /// </summary>
     public partial class MainWindow : Window
     {
+        Random rand = new Random();
+        int passwdLen;
+        char[] currentChar;
+        char[] specialChars;
+        string flags = "";
         public MainWindow()
         {
             InitializeComponent();
+            specialChars = new char[31];
+
+            for (int i = 33; i < 48; i++)
+            {
+                specialChars[i - 33] = (char)i;
+            }
+            for (int i = 58; i < 65; i++)
+            {
+                specialChars[i - 44] = (char)i;
+            }
+            for (int i = 91; i < 97; i++)
+            {
+                specialChars[i - 70] = (char)i;
+            }
+            for (int i = 123; i < 127; i++)
+            {
+                specialChars[i - 96] = (char)i;
+            }
+        }
+
+        char randomUpperChars()
+        {
+            char character = (char)rand.Next(65, 91);
+            return character;
+        }
+
+        char randomLowerChars()
+        {
+            char character = (char)rand.Next(97, 123);
+            return character;
+        }
+
+        char randomNumbers()
+        {
+            char character = (char)rand.Next(48, 58);
+            return character;
+        }
+
+        char randomSpecial()
+        {
+            return specialChars[rand.Next(31)];
+        }
+
+        void checkFlags()
+        {
+            if (upper.IsChecked == true) flags += "/u";
+            if (lower.IsChecked == true) flags += "/l";
+            if (numbers.IsChecked == true) flags += "/n";
+            if (special.IsChecked == true) flags += "/s";
+        }
+
+        private void generateButton_Click(object sender, RoutedEventArgs e)
+        {
+            passwdLen = Int16.Parse(passwdLength.Text);
+            currentChar = new char[passwdLen];
+            checkFlags();
+            if (flags.Contains("/u") && flags.Contains("/l") && flags.Contains("/n") && flags.Contains("/s"))
+            {
+                for (int i = 0; i < passwdLen; i++)
+                {
+                    switch (rand.Next(4))
+                    {
+                        case 0:
+                            currentChar[i] = randomUpperChars();
+                            break;
+                        case 1:
+                            currentChar[i] = randomLowerChars();
+                            break;
+                        case 2:
+                            currentChar[i] = randomNumbers();
+                            break;
+                        case 3:
+                            currentChar[i] = randomSpecial();
+                            break;
+                    }
+
+                }
+                passwdBox.Text = new string(currentChar);
+                alert.Content = "";
+            }
+            else alert.Content = "You have to check something!";
         }
     }
 }
